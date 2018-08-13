6d813e05070a80067da623a6d7563b74b02dab53
icanhazbroccoli <me@4pcbr.com>
2018-08-13 08:38:10 +0200

    Implemented a draft version of UDP receiver option enabling Evio library as the net backend

--- pkg/receiver/udp/udp.go
+++ pkg/receiver/udp/udp.go
@@ -7,6 +7,8 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/whiteboxio/flow/pkg/core"
 	"github.com/whiteboxio/flow/pkg/metrics"
+
+	"github.com/tidwall/evio"
 )
 
 const (
@@ -25,6 +27,14 @@ type UDP struct {
 }
 
 func NewUDP(name string, params core.Params) (core.Link, error) {
+
+	if lib, ok := params["lib"]; ok {
+		if lib.(string) == "evio" {
+			return NewEvioUDP(name, params)
+		}
+		log.Errorf("Unable to detect UDP backend library: %s", lib)
+	}
+
 	udpAddr, ok := params["bind_addr"]
 	if !ok {
 		return nil, fmt.Errorf("UDP receiver parameters are missing bind_addr")
@@ -44,6 +54,35 @@ func NewUDP(name string, params core.Params) (core.Link, error) {
 	udp.conn = conn
 
 	go udp.recv()
+
+	return udp, nil
+}
+
+func NewEvioUDP(name string, params core.Params) (core.Link, error) {
+	udp := &UDP{name, nil, core.NewConnector()}
+
+	udpAddr, ok := params["bind_addr"]
+	if !ok {
+		return nil, fmt.Errorf("UDP receiver parameters are missing bind_addr")
+	}
+
+	log.Infof("Starting a new evio handler")
+
+	var events evio.Events
+	events.Data = func(connId int, in []byte) (out []byte, action evio.Action) {
+		msg := core.NewMessage(nil, in)
+		if sendErr := udp.Send(msg); sendErr != nil {
+			log.Errorf("UDP failed to accept message: %s", sendErr.Error())
+		} else {
+			metrics.GetCounter("receiver.udp.sent").Inc(1)
+		}
+		return
+	}
+	go func() {
+		if err := evio.Serve(events, "udp://"+udpAddr.(string)); err != nil {
+			log.Errorf("Unable to connect evio listener: %s", err)
+		}
+	}()
 
 	return udp, nil
 }
