7240a9bb602adf531e5fe0e458cba316ab1c88ad
icanhazbroccoli <me@4pcbr.com>
2018-08-25 11:30:31 +0200

    Fixed BinHeap tree unit tests: the test case structure allows to specify multiple valid options

--- pkg/data/bin_heap_test.go
+++ pkg/data/bin_heap_test.go
@@ -9,15 +9,17 @@ func TestBinHeap_Insert(t *testing.T) {
 	tests := []struct {
 		name     string
 		elements map[string]uint32
-		expected []*BinHeapNode
+		expected [][]*BinHeapNode
 	}{
 		{
 			name: "Single node",
 			elements: map[string]uint32{
 				"A": 1,
 			},
-			expected: []*BinHeapNode{
-				&BinHeapNode{value: "A", weight: 1},
+			expected: [][]*BinHeapNode{
+				[]*BinHeapNode{
+					&BinHeapNode{value: "A", weight: 1},
+				},
 			},
 		},
 		{
@@ -25,9 +27,11 @@ func TestBinHeap_Insert(t *testing.T) {
 			elements: map[string]uint32{
 				"A": 1, "B": 2,
 			},
-			expected: []*BinHeapNode{
-				&BinHeapNode{value: "B", weight: 2},
-				&BinHeapNode{value: "A", weight: 1},
+			expected: [][]*BinHeapNode{
+				[]*BinHeapNode{
+					&BinHeapNode{value: "B", weight: 2},
+					&BinHeapNode{value: "A", weight: 1},
+				},
 			},
 		},
 		{
@@ -35,10 +39,17 @@ func TestBinHeap_Insert(t *testing.T) {
 			elements: map[string]uint32{
 				"A": 1, "B": 2, "C": 1,
 			},
-			expected: []*BinHeapNode{
-				&BinHeapNode{value: "B", weight: 2},
-				&BinHeapNode{value: "A", weight: 1},
-				&BinHeapNode{value: "C", weight: 1},
+			expected: [][]*BinHeapNode{
+				[]*BinHeapNode{
+					&BinHeapNode{value: "B", weight: 2},
+					&BinHeapNode{value: "A", weight: 1},
+					&BinHeapNode{value: "C", weight: 1},
+				},
+				[]*BinHeapNode{
+					&BinHeapNode{value: "B", weight: 2},
+					&BinHeapNode{value: "C", weight: 1},
+					&BinHeapNode{value: "A", weight: 1},
+				},
 			},
 		},
 	}
@@ -48,10 +59,14 @@ func TestBinHeap_Insert(t *testing.T) {
 			for v, p := range testCase.elements {
 				bh.Insert(p, v)
 			}
-			if !reflect.DeepEqual(bh.vals, testCase.expected) {
-				for ix, v := range bh.vals {
-					t.Logf("ix: %d, val: %+v", ix, v)
+			matchFound := false
+			for _, exp := range testCase.expected {
+				if reflect.DeepEqual(bh.vals, exp) {
+					matchFound = true
+					break
 				}
+			}
+			if !matchFound {
 				t.Errorf("Unexpected contents of the tree: got %+v, expected: %+v",
 					bh.vals, testCase.expected)
 			}
