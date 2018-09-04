b99e718001e9a954d8856304802b36a15bfc8e51
icanhazbroccoli <me@4pcbr.com>
2018-09-04 08:42:01 +0200

    Implemented config cache file IsValid method, which returns true if a file eists and it's ttl hasn't passed yet

--- pkg/config/cache_file/cache_file.go
+++ pkg/config/cache_file/cache_file.go
@@ -22,25 +22,9 @@ func New(path string, ttl time.Duration) (*CacheFile, error) {
 }
 
 func (f *CacheFile) Read() ([]byte, error) {
-	stat, err := os.Stat(f.path)
-
-	if os.IsNotExist(err) {
-		log.Debugf("File %s does not exist, can't read", f.path)
-		return nil, err
-	} else if err != nil {
-		log.Debugf("Failed to stat file %s: %s", f.path, err)
-		return nil, err
-	}
-
-	modTime := stat.ModTime()
-	modSince := time.Now().Sub(modTime)
-	fmt.Printf("mod: %s, now: %s, modSince: %f\n", modTime.String(),
-		time.Now().String(), modSince.Seconds())
-	if modSince > f.ttl {
-		errMsg := fmt.Sprintf("File %s has expired (TTL: %f, modified: %f seconds ago)",
-			f.path, f.ttl.Seconds(), modSince.Seconds())
-		log.Debugf(errMsg)
-		return nil, fmt.Errorf(errMsg)
+	valid, thisIsWhy := f.IsValid()
+	if !valid {
+		return nil, thisIsWhy
 	}
 
 	data, err := ioutil.ReadFile(f.path)
@@ -61,12 +45,29 @@ func (f *CacheFile) Consolidate(data []byte) error {
 	return nil
 }
 
-func (f *CacheFile) IsValid() bool {
-	// TODO
-	return false
+func (f *CacheFile) IsValid() (bool, error) {
+	stat, err := os.Stat(f.path)
+
+	if os.IsNotExist(err) {
+		log.Debugf("File %s does not exist, can't read", f.path)
+		return false, err
+	} else if err != nil {
+		log.Debugf("Failed to stat file %s: %s", f.path, err)
+		return false, err
+	}
+
+	modTime := stat.ModTime()
+	modSince := time.Now().Sub(modTime)
+	if modSince > f.ttl {
+		errMsg := fmt.Sprintf("File %s has expired (TTL: %f, modified: %f seconds ago)",
+			f.path, f.ttl.Seconds(), modSince.Seconds())
+		log.Debugf(errMsg)
+		return false, fmt.Errorf(errMsg)
+	}
+	return true, nil
 }
 
-func (f *CacheFile) Truncate() error {
+func (f *CacheFile) Invalidate() error {
 	//TODO
 	return nil
 }
--- pkg/config/cache_file/cache_file_test.go
+++ pkg/config/cache_file/cache_file_test.go
@@ -102,3 +102,71 @@ func TestCacheFile_Read(t *testing.T) {
 	}
 
 }
+
+func TestCacheFile_IsValid(t *testing.T) {
+	tests := []struct {
+		name      string
+		payload   []byte
+		ttl       time.Duration
+		sleep     time.Duration
+		expected  bool
+		reasonWhy string
+	}{
+		{
+			"Valid file",
+			[]byte("foo:bar"),
+			time.Hour,
+			0 * time.Second,
+			true,
+			"",
+		},
+		{
+			"Expired file",
+			[]byte("foo:bar"),
+			20 * time.Millisecond,
+			30 * time.Millisecond,
+			false,
+			"^File\\s.*\\shas\\sexpired.*",
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			tmpfile, err := ioutil.TempFile("/tmp", "flow-test-cache-file")
+			if err != nil {
+				t.Fatalf("Failed to create a new tmp file: %s", err)
+			}
+			defer os.Remove(tmpfile.Name())
+
+			if writeErr := ioutil.WriteFile(tmpfile.Name(), testCase.payload, 0644); writeErr != nil {
+				t.Fatalf("Failed to write to tmp file: %s", writeErr)
+			}
+
+			time.Sleep(testCase.sleep)
+
+			f, err := New(tmpfile.Name(), testCase.ttl)
+			if err != nil {
+				t.Fatalf("Failed to instantiate a new CacheFile: %s",
+					err)
+			}
+
+			valid, why := f.IsValid()
+
+			if testCase.expected != valid {
+				t.Fatalf("Unexpected validity check result: %t, want: %t. "+
+					"Reason message: %s", valid, testCase.expected, why)
+			}
+
+			if testCase.reasonWhy != "" {
+				match, err := regexp.Match(testCase.reasonWhy, []byte(why.Error()))
+				if err != nil {
+					t.Fatalf("Failed to compile a regex: %s", err)
+				}
+				if !match {
+					t.Fatalf("Wrong assert on error match: got: %s, want: %s",
+						why.Error(), testCase.reasonWhy)
+				}
+			}
+		})
+	}
+}
