4ba06ed71346f10554db0eea1879d1dca4540850
icanhazbroccoli <me@4pcbr.com>
2018-09-06 09:12:36 +0200

    Some progres on RemoteHttpFile module

--- pkg/util/file/remote_http_file/remote_http_file.go
+++ pkg/util/file/remote_http_file/remote_http_file.go
@@ -1,13 +1,25 @@
 package file
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/fsnotify/fsnotify"
 	vf "github.com/whiteboxio/flow/pkg/util/file/volatile_file"
 )
 
 type RemoteHttpFile struct {
+	shouldStop  bool
+	fetchedData []byte
+	lastErr     error
+	lastMod     string
+	lock        sync.Mutex
 	*vf.VolatileFile
 }
 
@@ -16,12 +28,19 @@ func New(path string) (*RemoteHttpFile, error) {
 	if err != nil {
 		return nil, err
 	}
-	rhf := &RemoteHttpFile{vol}
+	rhf := &RemoteHttpFile{
+		false,
+		nil,
+		nil,
+		"",
+		sync.Mutex{},
+		vol,
+	}
 	return rhf, nil
 }
 
 func (rhf *RemoteHttpFile) Deploy() error {
-	//TODO
+
 	return nil
 }
 
@@ -30,8 +49,59 @@ func (rhf *RemoteHttpFile) TearDown() error {
 	return nil
 }
 
+func (rhf *RemoteHttpFile) DoFetch() {
+	rhf.lock.Lock()
+	defer rhf.lock.Unlock()
+
+	resp, err := http.Get(rhf.GetPath())
+	defer resp.Body.Close()
+
+	if err != nil {
+		rhf.lastErr = err
+		return
+	}
+
+	if resp.StatusCode < http.StatusOK ||
+		resp.StatusCode >= http.StatusMultipleChoices {
+		rhf.lastErr = fmt.Errorf("Bad response status: %d", resp.StatusCode)
+	}
+	lms := resp.Header.Get("Last-Modified")
+	var tRemote, tLocal time.Time
+	if rhf.lastMod != "" {
+		tLocal, err = http.ParseTime(rhf.lastMod)
+		if err != nil {
+			log.Warnf("Failed to parse local Last-Modified header [%s]: %s",
+				lms, err)
+		}
+	}
+	if lms != "" {
+		tRemote, err = http.ParseTime(lms)
+		if err != nil {
+			log.Warnf("Failed to parse remote Last-Modified header [%s]: %s",
+				lms, err)
+		}
+		rhf.lastMod = lms
+	}
+	if tRemote.IsZero() || tRemote.After(tLocal) {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Warnf("Failed to read response body: %s", err)
+		}
+		if bytes.Compare(rhf.fetchedData, body) != 0 {
+			log.Info("Received an updated response")
+			rhf.fetchedData = body
+			rhf.GetNotifyChan() <- fsnotify.Event{Op: fsnotify.Write}
+		} else {
+			log.Infof("No effective change detected")
+		}
+		return
+	} else {
+		log.Infof("No changes detected since the recent update")
+	}
+}
+
 func (rhf *RemoteHttpFile) ReadData() (interface{}, error) {
-	//TODO
+
 	return nil, nil
 }
 
