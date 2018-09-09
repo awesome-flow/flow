7200ccf36848e4a780c8070422e89c48c13336a2
icanhazbroccoli <me@4pcbr.com>
2018-09-09 21:13:33 +0200

    Added a local jumphash implementation

---
+++ pkg/util/data/jump_hash.go
@@ -0,0 +1,17 @@
+package data
+
+// <3 Damian
+// https://medium.com/@dgryski/consistent-hashing-algorithmic-tradeoffs-ef6b8e2fcae8
+
+func JumpHash(key uint64, numBuckets int) int32 {
+	var (
+		b int64 = -1
+		j int64
+	)
+	for j < int64(numBuckets) {
+		b = j
+		key = key*28629335557779417 + 1
+		j = int64(float64(b+1) * (float64(1<<31) / float64(key>>33+1)))
+	}
+	return int32(b)
+}
