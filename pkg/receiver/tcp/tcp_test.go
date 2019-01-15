fc030800d7122225ac47a5107f9c15f9fc50e4b5
Oleg Sidorov <oleg.sidorov@booking.com>
2019-01-15 12:16:20 +0100

    Fixed detected race conditions in tcp receiver

--- pkg/receiver/tcp/tcp_test.go
+++ pkg/receiver/tcp/tcp_test.go
@@ -1,51 +1,60 @@
 package receiver
 
 import (
-	"fmt"
 	"net"
-	"strings"
+	"reflect"
 	"testing"
 	"time"
 
 	"github.com/awesome-flow/flow/pkg/core"
+	testutils "github.com/awesome-flow/flow/pkg/util/test"
 )
 
-type A struct {
-	lastMsg []byte
-	*core.Connector
-}
-
-func NewA() *A {
-	return &A{nil, core.NewConnector()}
-}
-
-func (a *A) Recv(msg *core.Message) error {
-	fmt.Printf("A received a message\n")
-	a.lastMsg = msg.Payload
-	return msg.AckDone()
-}
+const (
+	DefaultMessageSize = 1024
+)
 
 func TestTCP_recv(t *testing.T) {
 	tcpAddr := ":7102"
-	payload := "hello world\r\n"
+
+	newline := []byte{'\r', '\n'}
+	payload := testutils.RandStringBytes(DefaultMessageSize)
+
 	tcp, err := New("test_tcp", core.Params{"bind_addr": tcpAddr}, core.NewContext())
 	if err != nil {
 		t.Fatalf("Failed to start a TCP listener: %s", err)
 	}
-	rcvLink := NewA()
+	rcvLink := testutils.NewRememberAndReply("rar", testutils.ReplyDone)
 	tcp.ConnectTo(rcvLink)
+
 	conn, connErr := net.DialTimeout("tcp", tcpAddr, 1*time.Second)
 	if connErr != nil {
 		t.Fatalf("Failed to open a TCP connection: %s", connErr)
 	}
-	n, writeErr := conn.Write([]byte(payload))
+
+	_, writeErr := conn.Write(append(payload, newline...))
 	if writeErr != nil {
 		t.Fatalf("Failed to write TCP data: %s", writeErr)
 	}
-	t.Logf("Sent %d bytes over the network", n)
-	time.Sleep(10 * time.Millisecond)
-	if string(rcvLink.lastMsg) != strings.Trim(payload, "\r\n") {
-		t.Fatalf("Unexpected receiver last message: %q, expected: %q",
-			rcvLink.lastMsg, payload)
+
+	received := make(chan struct{})
+	go func() {
+		for {
+			if rcvLink.LastMsg() != nil {
+				received <- struct{}{}
+				return
+			}
+		}
+	}()
+
+	select {
+	case <-received:
+	case <-time.After(10 * time.Millisecond):
+		t.Fatalf("Timed out to receive the message")
+	}
+
+	if !reflect.DeepEqual(rcvLink.LastMsg().Payload, payload) {
+		t.Fatalf("Unexpected receiver last message: got %q, want: %q",
+			rcvLink.LastMsg().Payload, payload)
 	}
 }
