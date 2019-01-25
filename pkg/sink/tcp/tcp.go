c2299def4041b0ead758405af2911c015cf628a6
Oleg Sidorov <me@whitebox.io>
2019-01-25 22:52:07 +0100

    Implemented udp and tcp sink docker-compose fragments and extended Fragments with DockerComposeBuilder func

--- pkg/sink/tcp/tcp.go
+++ pkg/sink/tcp/tcp.go
@@ -33,6 +33,7 @@ func New(name string, params core.Params, context *core.Context) (core.Link, err
 	if _, ok := params["bind_addr"]; !ok {
 		return nil, fmt.Errorf("TCP sink parameters are missing bind_addr")
 	}
+
 	tcpaddr, err := net.ResolveTCPAddr("tcp", params["bind_addr"].(string))
 	if err != nil {
 		return nil, err
