782d7842f3c86a65a583cabe80498f14bd3a5541
Oleg Sidorov <me@whitebox.io>
2019-02-02 12:24:02 +0100

    TCP receiver refactoring: performance optimistions

--- pkg/receiver/tcp/tcp.go
+++ pkg/receiver/tcp/tcp.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"net"
-	"strings"
 	"time"
 
 	"github.com/awesome-flow/flow/pkg/core"
@@ -16,32 +15,63 @@ import (
 )
 
 const (
-	ConnReadTimeout  = 1 * time.Second
-	ConnWriteTimeout = 1 * time.Second
+	ConnReadTimeout  = 50 * time.Millisecond
+	ConnWriteTimeout = 50 * time.Millisecond
 )
-
-type replyMode uint8
 
 const (
 	replyModeSilent replyMode = iota
 	replyModeTalkative
 )
 
+const (
+	TcpMetricsPref = "receiver.tcp.msg"
+
+	TcpMetricsConnFailed = "receiver.tcp.conn.failed"
+	TcpMetricsConnOpnd   = "receiver.tcp.conn.opened"
+	TcpMetricsConnClosed = "receiver.tcp.conn.closed"
+
+	TcpMetricsMsgFailed  = "receiver.tcp.msg.failed"
+	TcpMetricsMsgRcvd    = "receiver.tcp.msg.received"
+	TcpMetricsMsgTimeout = "receiver.tcp.msg.timeout"
+)
+
+var TcpMetricsMsgStatusMap = map[core.MsgStatus]string{
+	core.MsgStatusDone:        TcpMetricsPref + ".sent_ok",
+	core.MsgStatusFailed:      TcpMetricsPref + ".sent_failed",
+	core.MsgStatusInvalid:     TcpMetricsPref + ".sent_invalid",
+	core.MsgStatusPartialSend: TcpMetricsPref + ".sent_part",
+	core.MsgStatusTimedOut:    TcpMetricsPref + ".sent_timeout",
+	core.MsgStatusThrottled:   TcpMetricsPref + ".sent_throttled",
+	core.MsgStatusUnroutable:  TcpMetricsPref + ".sent_unroutable",
+}
+
+var MsgStatusToTcpResp = map[core.MsgStatus][]byte{
+	core.MsgStatusDone:        TcpRespOk,
+	core.MsgStatusPartialSend: TcpRespPsnt,
+	core.MsgStatusInvalid:     TcpRespInvd,
+	core.MsgStatusFailed:      TcpRespFail,
+	core.MsgStatusTimedOut:    TcpRespTime,
+	core.MsgStatusUnroutable:  TcpRespUnrt,
+	core.MsgStatusThrottled:   TcpRespThrt,
+}
+
 var (
-	TcpRespAcpt = []byte("ACCEPTED")
-	TcpRespSent = []byte("SENT")
-	TcpRespPsnt = []byte("PART_SENT")
-	TcpRespFail = []byte("FAILED")
-	TcpRespInvd = []byte("INVALID")
-	TcpRespTime = []byte("TIMEOUT")
-	TcpRespUnrt = []byte("UNROUTABLE")
-	TcpRespThrt = []byte("THROTTLED")
+	TcpRespFail = []byte("FAILED\r\n")
+	TcpRespInvd = []byte("INVALID\r\n")
+	TcpRespPsnt = []byte("PARTSENT\r\n")
+	TcpRespOk   = []byte("OK\r\n")
+	TcpRespTime = []byte("TIMEOUT\r\n")
+	TcpRespThrt = []byte("THROTTLED\r\n")
+	TcpRespUnrt = []byte("UNROUTABLE\r\n")
 
 	ErrMalformedPacket = fmt.Errorf("Malformed packet")
 	ErrEmptyBody       = fmt.Errorf("Empty message body")
 
-	TcpMsgSendTimeout = 100 * time.Millisecond
+	MsgSendTimeout = 100 * time.Millisecond
 )
+
+type replyMode uint8
 
 type TCP struct {
 	Name string
@@ -128,7 +158,7 @@ func (tcp *TCP) handleListener() {
 func (tcp *TCP) handleConnection(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 
-	metrics.GetCounter("receiver.tcp.conn.opened").Inc(1)
+	metrics.GetCounter(TcpMetricsConnOpnd).Inc(1)
 
 	for {
 		conn.SetReadDeadline(time.Now().Add(ConnReadTimeout))
@@ -138,43 +168,33 @@ func (tcp *TCP) handleConnection(conn net.Conn) {
 			break
 		}
 
-		metrics.GetCounter("receiver.tcp.msg.received").Inc(1)
+		metrics.GetCounter(TcpMetricsMsgRcvd).Inc(1)
 
 		if err != nil && err != io.EOF {
 			log.Errorf("TCP receiver failed to read data: %s", err)
-			metrics.GetCounter("receiver.tcp.conn.failed").Inc(1)
-			conn.SetWriteDeadline(time.Now().Add(ConnWriteTimeout))
-			conn.Write(TcpRespInvd)
+			metrics.GetCounter(TcpMetricsConnFailed).Inc(1)
+			tcp.replyWith(conn, TcpRespInvd)
 			conn.Close()
-			metrics.GetCounter("receiver.tcp.conn.closed").Inc(1)
+			metrics.GetCounter(TcpMetricsConnClosed).Inc(1)
 			return
 		}
 
-		data = bytes.TrimRight(data, "\r\n")
-		msg := core.NewMessage(data)
+		msg := core.NewMessage(bytes.TrimRight(data, "\r\n"))
 
 		if sendErr := tcp.Send(msg); sendErr != nil {
-			metrics.GetCounter("receiver.tcp.msg.failed").Inc(1)
+			metrics.GetCounter(TcpMetricsMsgFailed).Inc(1)
 			log.Errorf("Failed to send message: %s", sendErr)
 			tcp.replyWith(conn, TcpRespFail)
 			continue
 		}
 
-		sync, ok := msg.Meta("sync")
-		isSync := ok && (sync.(string) == "true" || sync.(string) == "1")
-		if !isSync {
-			metrics.GetCounter("receiver.tcp.msg.accepted").Inc(1)
-			tcp.replyWith(conn, TcpRespAcpt)
-			continue
-		}
-
 		select {
 		case s := <-msg.GetAckCh():
-			metrics.GetCounter(
-				"receiver.tcp.msg.sent_" + strings.ToLower(string(status2resp(s)))).Inc(1)
-			tcp.replyWith(conn, status2resp(s))
-		case <-time.After(TcpMsgSendTimeout):
-			metrics.GetCounter("receiver.tcp.msg.timed_out").Inc(1)
+			metrics.GetCounter(TcpMetricsMsgStatusMap[s]).Inc(1)
+			conn.SetWriteDeadline(time.Now().Add(ConnWriteTimeout))
+			tcp.replyWith(conn, MsgStatusToTcpResp[s])
+		case <-time.After(MsgSendTimeout):
+			metrics.GetCounter(TcpMetricsMsgTimeout).Inc(1)
 			tcp.replyWith(conn, TcpRespTime)
 		}
 
@@ -182,7 +202,7 @@ func (tcp *TCP) handleConnection(conn net.Conn) {
 			break
 		}
 	}
-	metrics.GetCounter("receiver.tcp.conn.closed").Inc(1)
+	metrics.GetCounter(TcpMetricsConnClosed).Inc(1)
 	conn.Close()
 }
 
@@ -192,25 +212,4 @@ func (tcp *TCP) replyWith(conn net.Conn, reply []byte) {
 	}
 	conn.SetWriteDeadline(time.Now().Add(ConnWriteTimeout))
 	conn.Write(reply)
-}
-
-func status2resp(s core.MsgStatus) []byte {
-	switch s {
-	case core.MsgStatusDone:
-		return TcpRespSent
-	case core.MsgStatusPartialSend:
-		return TcpRespPsnt
-	case core.MsgStatusInvalid:
-		return TcpRespInvd
-	case core.MsgStatusFailed:
-		return TcpRespFail
-	case core.MsgStatusTimedOut:
-		return TcpRespTime
-	case core.MsgStatusUnroutable:
-		return TcpRespUnrt
-	case core.MsgStatusThrottled:
-		return TcpRespThrt
-	default:
-		return []byte("This should not happen")
-	}
 }
