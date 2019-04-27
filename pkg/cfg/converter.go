f2331dd0e11cbb58f313a50b36eab526d4ad9c5e
Oleg Sidorov <me@whitebox.io>
2019-04-27 10:34:31 +0200

    Introduced composite attribute converters operating as a chain with a specified behavior (and, or, none, first wins, last wins)

--- pkg/cfg/converter.go
+++ pkg/cfg/converter.go
@@ -1,6 +1,8 @@
 package cfg
 
-import "strconv"
+import (
+	"strconv"
+)
 
 type Converter interface {
 	Convert(kv *KeyValue) (*KeyValue, bool)
@@ -14,13 +16,55 @@ func (id *IdentityConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
 	return kv, true
 }
 
-type PtrToIntConverter struct{}
+type IntPtrToIntConverter struct{}
 
-var _ Converter = (*PtrToIntConverter)(nil)
+var _ Converter = (*IntPtrToIntConverter)(nil)
 
-func (pi *PtrToIntConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
+func (pi *IntPtrToIntConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
 	if pv, ok := kv.Value.(*int); ok {
 		return &KeyValue{kv.Key, *pv}, true
+	}
+	return nil, false
+}
+
+type BoolPtrToBoolConverter struct{}
+
+var _ Converter = (*BoolPtrToBoolConverter)(nil)
+
+func (pb *BoolPtrToBoolConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
+	if pv, ok := kv.Value.(*bool); ok {
+		return &KeyValue{kv.Key, *pv}, true
+	}
+	return nil, false
+}
+
+type StrPtrToStrConverter struct{}
+
+var _ Converter = (*StrPtrToStrConverter)(nil)
+
+func (sps *StrPtrToStrConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
+	if spv, ok := kv.Value.(*string); ok {
+		return &KeyValue{kv.Key, *spv}, true
+	}
+	return nil, false
+}
+
+type StrToBoolConverter struct{}
+
+var _ Converter = (*StrToBoolConverter)(nil)
+
+func (sb *StrToBoolConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
+	if sv, ok := kv.Value.(string); ok {
+		switch sv {
+		case "true":
+		case "1":
+		case "y":
+			return &KeyValue{kv.Key, true}, true
+		case "false":
+		case "0":
+		case "n":
+			return &KeyValue{kv.Key, false}, true
+		}
 	}
 	return nil, false
 }
@@ -39,13 +83,16 @@ func (si *StrToIntConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
 	return nil, false
 }
 
-type StrPtrToStrConverter struct{}
+type IntToBoolConverter struct{}
 
-var _ Converter = (*StrPtrToStrConverter)(nil)
+var _ Converter = (*IntToBoolConverter)(nil)
 
-func (sps *StrPtrToStrConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
-	if spv, ok := kv.Value.(*string); ok {
-		return &KeyValue{kv.Key, *spv}, true
+func (ib *IntToBoolConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
+	if mv, ok := kv.Value.(int); ok {
+		if mv == 0 {
+			return &KeyValue{kv.Key, false}, true
+		}
+		return &KeyValue{kv.Key, true}, true
 	}
 	return nil, false
 }
@@ -59,4 +106,133 @@ func (is *IntToStrConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
 		return &KeyValue{kv.Key, strconv.Itoa(iv)}, true
 	}
 	return nil, false
+}
+
+type IfIntConverter struct{}
+
+var _ Converter = (*IfIntConverter)(nil)
+
+func (ii *IfIntConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
+	if _, ok := kv.Value.(int); ok {
+		return kv, true
+	}
+	return nil, false
+}
+
+type IfStrConverter struct{}
+
+var _ Converter = (*IfStrConverter)(nil)
+
+func (is *IfStrConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
+	if _, ok := kv.Value.(string); ok {
+		return kv, true
+	}
+	return nil, false
+}
+
+type IfBoolConverter struct{}
+
+var _ Converter = (*IfBoolConverter)(nil)
+
+func (ib *IfBoolConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
+	if _, ok := kv.Value.(bool); ok {
+		return kv, true
+	}
+	return nil, false
+}
+
+//======== Composite converters =======
+
+type CompositionStrategy uint8
+
+const (
+	CompNone CompositionStrategy = iota
+	CompAnd
+	CompOr
+	CompFirst
+	CompLast
+)
+
+type CompositeConverter struct {
+	strategy   CompositionStrategy
+	converters []Converter
+}
+
+func NewCompositeConverter(strategy CompositionStrategy, convs ...Converter) *CompositeConverter {
+	return &CompositeConverter{
+		strategy:   strategy,
+		converters: convs,
+	}
+}
+
+func (cc *CompositeConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
+	switch cc.strategy {
+	case CompNone:
+		return kv, true
+	case CompAnd:
+		mkv := kv
+		var ok bool
+		for _, conv := range cc.converters {
+			mkv, ok = conv.Convert(mkv)
+			if !ok {
+				return nil, false
+			}
+		}
+		return mkv, ok
+	case CompFirst, CompOr:
+		for _, conv := range cc.converters {
+			if mkv, ok := conv.Convert(kv); ok {
+				return mkv, ok
+			}
+		}
+		return nil, false
+	case CompLast:
+		var res *KeyValue
+		var resok bool
+		for _, conv := range cc.converters {
+			if mkv, ok := conv.Convert(kv); ok {
+				res = mkv
+				resok = ok
+			}
+		}
+		return res, resok
+	default:
+		panic("this should not happen")
+	}
+	return nil, false
+}
+
+var (
+	Identity *IdentityConverter
+
+	BoolPtrToBool *BoolPtrToBoolConverter
+	IntPtrToInt   *IntPtrToIntConverter
+	StrPtrToStr   *StrPtrToStrConverter
+
+	IntToBool *IntToBoolConverter
+	IntToStr  *IntToStrConverter
+	StrToBool *StrToBoolConverter
+	StrToInt  *StrToIntConverter
+
+	IfInt  *IfIntConverter
+	IfStr  *IfStrConverter
+	IfBool *IfBoolConverter
+
+	IntOrIntPtr   *CompositeConverter
+	StrOrStrPtr   *CompositeConverter
+	BoolOrBoolPtr *CompositeConverter
+
+	ToInt  *CompositeConverter
+	ToStr  *CompositeConverter
+	ToBool *CompositeConverter
+)
+
+func init() {
+	IntOrIntPtr = NewCompositeConverter(CompOr, IfInt, IntPtrToInt)
+	StrOrStrPtr = NewCompositeConverter(CompOr, IfStr, StrPtrToStr)
+	BoolOrBoolPtr = NewCompositeConverter(CompOr, IfBool, BoolPtrToBool)
+
+	ToInt = NewCompositeConverter(CompOr, IntOrIntPtr, StrToInt)
+	ToStr = NewCompositeConverter(CompOr, StrOrStrPtr, IntToStr)
+	ToBool = NewCompositeConverter(CompOr, BoolOrBoolPtr, StrToBool, IntToBool)
 }
---
+++ pkg/cfg/converter_test.go
@@ -0,0 +1,670 @@
+package cfg
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func Test_IdentityConverter(t *testing.T) {
+	tests := []struct {
+		inVal   interface{}
+		outVal  interface{}
+		outFlag bool
+	}{
+		{1, 1, true},
+		{nil, nil, true},
+		{'a', 'a', true},
+		{"asdf", "asdf", true},
+		{struct{}{}, struct{}{}, true},
+	}
+
+	for ix, testCase := range tests {
+		t.Run(fmt.Sprintf("Test #%d", ix), func(t *testing.T) {
+			in := &KeyValue{nil, testCase.inVal}
+			out, ok := Identity.Convert(in)
+			if ok != testCase.outFlag {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.outFlag, ok)
+			}
+			if !ok {
+				return
+			}
+			if out == nil && testCase.outVal != nil {
+				t.Errorf("Expected a non-nil result, got nil")
+			}
+			if !reflect.DeepEqual(testCase.outVal, out.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.outVal, out.Value)
+			}
+		})
+	}
+}
+
+func Test_IntToStrConverter(t *testing.T) {
+	tests := []struct {
+		inVal   interface{}
+		outVal  interface{}
+		outFlag bool
+	}{
+		{1, "1", true},
+		{-1, "-1", true},
+		{nil, nil, false},
+		{'a', nil, false},
+		{"asdf", nil, false},
+		{struct{}{}, nil, false},
+	}
+
+	for ix, testCase := range tests {
+		t.Run(fmt.Sprintf("Test #%d", ix), func(t *testing.T) {
+			in := &KeyValue{nil, testCase.inVal}
+			out, ok := IntToStr.Convert(in)
+			if ok != testCase.outFlag {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.outFlag, ok)
+			}
+			if !ok {
+				return
+			}
+			if out == nil && testCase.outVal != nil {
+				t.Errorf("Expected a non-nil result, got nil")
+			}
+			if !reflect.DeepEqual(testCase.outVal, out.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.outVal, out.Value)
+			}
+		})
+	}
+}
+
+func intptr(v int) *int { return &v }
+func Test_IntPtrToIntConverter(t *testing.T) {
+	tests := []struct {
+		inVal   interface{}
+		outVal  interface{}
+		outFlag bool
+	}{
+		{1, nil, false},
+		{intptr(42), 42, true},
+		{nil, nil, false},
+		{'a', nil, false},
+		{"asdf", nil, false},
+		{struct{}{}, nil, false},
+	}
+
+	for ix, testCase := range tests {
+		t.Run(fmt.Sprintf("Test #%d", ix), func(t *testing.T) {
+			in := &KeyValue{nil, testCase.inVal}
+			out, ok := IntPtrToInt.Convert(in)
+			if ok != testCase.outFlag {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.outFlag, ok)
+			}
+			if !ok {
+				return
+			}
+			if out == nil && testCase.outVal != nil {
+				t.Errorf("Expected a non-nil result, got nil")
+			}
+			if !reflect.DeepEqual(testCase.outVal, out.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.outVal, out.Value)
+			}
+		})
+	}
+}
+
+func strptr(v string) *string { return &v }
+func Test_StrPtrToStrConverter(t *testing.T) {
+	tests := []struct {
+		inVal   interface{}
+		outVal  interface{}
+		outFlag bool
+	}{
+		{1, nil, false},
+		{nil, nil, false},
+		{'a', nil, false},
+		{"asdf", nil, false},
+		{strptr("asdf"), "asdf", true},
+		{struct{}{}, nil, false},
+	}
+
+	for ix, testCase := range tests {
+		t.Run(fmt.Sprintf("Test #%d", ix), func(t *testing.T) {
+			in := &KeyValue{nil, testCase.inVal}
+			out, ok := StrPtrToStr.Convert(in)
+			if ok != testCase.outFlag {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.outFlag, ok)
+			}
+			if !ok {
+				return
+			}
+			if out == nil && testCase.outVal != nil {
+				t.Errorf("Expected a non-nil result, got nil")
+			}
+			if !reflect.DeepEqual(testCase.outVal, out.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.outVal, out.Value)
+			}
+		})
+	}
+}
+
+func boolptr(v bool) *bool { return &v }
+func Test_BoolPtrToBoolConverter(t *testing.T) {
+	tests := []struct {
+		inVal   interface{}
+		outVal  interface{}
+		outFlag bool
+	}{
+		{true, nil, false},
+		{boolptr(true), true, true},
+		{boolptr(false), false, true},
+		{nil, nil, false},
+	}
+
+	for ix, testCase := range tests {
+		t.Run(fmt.Sprintf("Test #%d", ix), func(t *testing.T) {
+			in := &KeyValue{nil, testCase.inVal}
+			out, ok := BoolPtrToBool.Convert(in)
+			if ok != testCase.outFlag {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.outFlag, ok)
+			}
+			if !ok {
+				return
+			}
+			if out == nil && testCase.outVal != nil {
+				t.Errorf("Expected a non-nil result, got nil")
+			}
+			if !reflect.DeepEqual(testCase.outVal, out.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.outVal, out.Value)
+			}
+		})
+	}
+}
+
+func Test_StrToIntConverter(t *testing.T) {
+	tests := []struct {
+		inVal   interface{}
+		outVal  interface{}
+		outFlag bool
+	}{
+		{1, nil, false},
+		{"1", 1, true},
+		{"-1", -1, true},
+		{"1234567890", 1234567890, true},
+		{"asdf", nil, false},
+		{'1', nil, false},
+	}
+
+	for ix, testCase := range tests {
+		t.Run(fmt.Sprintf("Test #%d", ix), func(t *testing.T) {
+			in := &KeyValue{nil, testCase.inVal}
+			out, ok := StrToInt.Convert(in)
+			if ok != testCase.outFlag {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.outFlag, ok)
+			}
+			if !ok {
+				return
+			}
+			if out == nil && testCase.outVal != nil {
+				t.Errorf("Expected a non-nil result, got nil")
+			}
+			if !reflect.DeepEqual(testCase.outVal, out.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.outVal, out.Value)
+			}
+		})
+	}
+}
+
+func Test_IfIntConverter(t *testing.T) {
+	tests := []struct {
+		inVal   interface{}
+		outVal  interface{}
+		outFlag bool
+	}{
+		{1, 1, true},
+		{-1, -1, true},
+		{0, 0, true},
+		{"asdf", nil, false},
+		{intptr(1), nil, false},
+		{"1", nil, false},
+		{nil, nil, false},
+	}
+
+	for ix, testCase := range tests {
+		t.Run(fmt.Sprintf("Test #%d", ix), func(t *testing.T) {
+			in := &KeyValue{nil, testCase.inVal}
+			out, ok := IfInt.Convert(in)
+			if ok != testCase.outFlag {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.outFlag, ok)
+			}
+			if !ok {
+				return
+			}
+			if out == nil && testCase.outVal != nil {
+				t.Errorf("Expected a non-nil result, got nil")
+			}
+			if !reflect.DeepEqual(testCase.outVal, out.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.outVal, out.Value)
+			}
+		})
+	}
+}
+
+func Test_IfStrConverter(t *testing.T) {
+	tests := []struct {
+		inVal   interface{}
+		outVal  interface{}
+		outFlag bool
+	}{
+		{1, nil, false},
+		{"asdf", "asdf", true},
+		{strptr("asdf"), nil, false},
+		{'a', nil, false},
+		{nil, nil, false},
+	}
+
+	for ix, testCase := range tests {
+		t.Run(fmt.Sprintf("Test #%d", ix), func(t *testing.T) {
+			in := &KeyValue{nil, testCase.inVal}
+			out, ok := IfStr.Convert(in)
+			if ok != testCase.outFlag {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.outFlag, ok)
+			}
+			if !ok {
+				return
+			}
+			if out == nil && testCase.outVal != nil {
+				t.Errorf("Expected a non-nil result, got nil")
+			}
+			if !reflect.DeepEqual(testCase.outVal, out.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.outVal, out.Value)
+			}
+		})
+	}
+}
+
+func Test_IfBoolConverter(t *testing.T) {
+	tests := []struct {
+		inVal   interface{}
+		outVal  interface{}
+		outFlag bool
+	}{
+		{true, true, true},
+		{false, false, true},
+		{nil, nil, false},
+		{"true", nil, false},
+		{1, nil, false},
+		{0, nil, false},
+	}
+
+	for ix, testCase := range tests {
+		t.Run(fmt.Sprintf("Test #%d", ix), func(t *testing.T) {
+			in := &KeyValue{nil, testCase.inVal}
+			out, ok := IfBool.Convert(in)
+			if ok != testCase.outFlag {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.outFlag, ok)
+			}
+			if !ok {
+				return
+			}
+			if out == nil && testCase.outVal != nil {
+				t.Errorf("Expected a non-nil result, got nil")
+			}
+			if !reflect.DeepEqual(testCase.outVal, out.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.outVal, out.Value)
+			}
+		})
+	}
+}
+
+type convAct struct {
+	res Value
+	ok  bool
+}
+
+type testConverter struct {
+	conv func(kv *KeyValue) (*KeyValue, bool)
+}
+
+var _ Converter = (*testConverter)(nil)
+
+func NewTestConverter(act convAct) *testConverter {
+	return &testConverter{
+		conv: func(kv *KeyValue) (*KeyValue, bool) {
+			if act.ok {
+				return &KeyValue{kv.Key, act.res}, act.ok
+			}
+			return nil, false
+		},
+	}
+}
+
+func (tc *testConverter) Convert(kv *KeyValue) (*KeyValue, bool) {
+	return tc.conv(kv)
+}
+
+func Test_CompositeConverter_CompAnd(t *testing.T) {
+	tests := []struct {
+		name   string
+		chain  []convAct
+		expVal Value
+		expOk  bool
+	}{
+		{
+			"Empty chain",
+			[]convAct{},
+			nil, false,
+		},
+		{
+			"1 positive",
+			[]convAct{
+				convAct{1, true},
+			},
+			1, true,
+		},
+		{
+			"2 positive",
+			[]convAct{
+				convAct{1, true},
+				convAct{2, true},
+			},
+			2, true,
+		},
+		{
+			"1 negative",
+			[]convAct{
+				convAct{nil, false},
+			},
+			nil, false,
+		},
+		{
+			"1 positive 1 negative",
+			[]convAct{
+				convAct{1, true},
+				convAct{nil, false},
+			},
+			nil, false,
+		},
+		{
+			"1 negative 1 positive",
+			[]convAct{
+				convAct{nil, false},
+				convAct{1, true},
+			},
+			nil, false,
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			convChain := make([]Converter, 0, len(testCase.chain))
+			for _, act := range testCase.chain {
+				convChain = append(convChain, NewTestConverter(act))
+			}
+
+			comp := NewCompositeConverter(CompAnd, convChain...)
+			// None of the converters react to the input kv, so
+			// passing a nil value
+			got, gotOk := comp.Convert(&KeyValue{nil, nil})
+			if gotOk != testCase.expOk {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.expOk, gotOk)
+			}
+			if !gotOk {
+				return
+			}
+			if !reflect.DeepEqual(testCase.expVal, got.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.expVal, got.Value)
+			}
+		})
+	}
+}
+
+func Test_CompositeConverter_CompOr(t *testing.T) {
+	tests := []struct {
+		name   string
+		chain  []convAct
+		expVal Value
+		expOk  bool
+	}{
+		{
+			"Empty chain",
+			[]convAct{},
+			nil, false,
+		},
+		{
+			"1 positive",
+			[]convAct{
+				convAct{1, true},
+			},
+			1, true,
+		},
+		{
+			"2 positive",
+			[]convAct{
+				convAct{1, true},
+				convAct{2, true},
+			},
+			1, true,
+		},
+		{
+			"1 negative",
+			[]convAct{
+				convAct{nil, false},
+			},
+			nil, false,
+		},
+		{
+			"1 positive 1 negative",
+			[]convAct{
+				convAct{1, true},
+				convAct{nil, false},
+			},
+			1, true,
+		},
+		{
+			"1 negative 1 positive",
+			[]convAct{
+				convAct{nil, false},
+				convAct{2, true},
+			},
+			2, true,
+		},
+		{
+			"1 negative 2 positives",
+			[]convAct{
+				convAct{nil, false},
+				convAct{1, true},
+				convAct{2, true},
+			},
+			1, true,
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			convChain := make([]Converter, 0, len(testCase.chain))
+			for _, act := range testCase.chain {
+				convChain = append(convChain, NewTestConverter(act))
+			}
+
+			comp := NewCompositeConverter(CompOr, convChain...)
+			// None of the converters react to the input kv, so
+			// passing a nil value
+			got, gotOk := comp.Convert(&KeyValue{nil, nil})
+			if gotOk != testCase.expOk {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.expOk, gotOk)
+			}
+			if !gotOk {
+				return
+			}
+			if !reflect.DeepEqual(testCase.expVal, got.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.expVal, got.Value)
+			}
+		})
+	}
+}
+
+func Test_CompositeConverter_CompFirst(t *testing.T) {
+	tests := []struct {
+		name   string
+		chain  []convAct
+		expVal Value
+		expOk  bool
+	}{
+		{
+			"Empty chain",
+			[]convAct{},
+			nil, false,
+		},
+		{
+			"1 positive",
+			[]convAct{
+				convAct{1, true},
+			},
+			1, true,
+		},
+		{
+			"2 positive",
+			[]convAct{
+				convAct{1, true},
+				convAct{2, true},
+			},
+			1, true,
+		},
+		{
+			"1 negative",
+			[]convAct{
+				convAct{nil, false},
+			},
+			nil, false,
+		},
+		{
+			"1 positive 1 negative",
+			[]convAct{
+				convAct{1, true},
+				convAct{nil, false},
+			},
+			1, true,
+		},
+		{
+			"1 negative 1 positive",
+			[]convAct{
+				convAct{nil, false},
+				convAct{2, true},
+			},
+			2, true,
+		},
+		{
+			"1 negative 2 positives",
+			[]convAct{
+				convAct{nil, false},
+				convAct{1, true},
+				convAct{2, true},
+			},
+			1, true,
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			convChain := make([]Converter, 0, len(testCase.chain))
+			for _, act := range testCase.chain {
+				convChain = append(convChain, NewTestConverter(act))
+			}
+
+			comp := NewCompositeConverter(CompFirst, convChain...)
+			// None of the converters react to the input kv, so
+			// passing a nil value
+			got, gotOk := comp.Convert(&KeyValue{nil, nil})
+			if gotOk != testCase.expOk {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.expOk, gotOk)
+			}
+			if !gotOk {
+				return
+			}
+			if !reflect.DeepEqual(testCase.expVal, got.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.expVal, got.Value)
+			}
+		})
+	}
+}
+
+func Test_CompositeConverter_CompLast(t *testing.T) {
+	tests := []struct {
+		name   string
+		chain  []convAct
+		expVal Value
+		expOk  bool
+	}{
+		{
+			"Empty chain",
+			[]convAct{},
+			nil, false,
+		},
+		{
+			"1 positive",
+			[]convAct{
+				convAct{1, true},
+			},
+			1, true,
+		},
+		{
+			"2 positive",
+			[]convAct{
+				convAct{1, true},
+				convAct{2, true},
+			},
+			2, true,
+		},
+		{
+			"1 negative",
+			[]convAct{
+				convAct{nil, false},
+			},
+			nil, false,
+		},
+		{
+			"1 positive 1 negative",
+			[]convAct{
+				convAct{1, true},
+				convAct{nil, false},
+			},
+			1, true,
+		},
+		{
+			"1 negative 1 positive",
+			[]convAct{
+				convAct{nil, false},
+				convAct{2, true},
+			},
+			2, true,
+		},
+		{
+			"1 negative 2 positives",
+			[]convAct{
+				convAct{nil, false},
+				convAct{1, true},
+				convAct{2, true},
+			},
+			2, true,
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			convChain := make([]Converter, 0, len(testCase.chain))
+			for _, act := range testCase.chain {
+				convChain = append(convChain, NewTestConverter(act))
+			}
+
+			comp := NewCompositeConverter(CompLast, convChain...)
+			// None of the converters react to the input kv, so
+			// passing a nil value
+			got, gotOk := comp.Convert(&KeyValue{nil, nil})
+			if gotOk != testCase.expOk {
+				t.Errorf("Unexpected Convert flag: want: %t, got: %t", testCase.expOk, gotOk)
+			}
+			if !gotOk {
+				return
+			}
+			if !reflect.DeepEqual(testCase.expVal, got.Value) {
+				t.Errorf("Unexpected Convert value: want: %#v, got: %#v", testCase.expVal, got.Value)
+			}
+		})
+	}
+}
