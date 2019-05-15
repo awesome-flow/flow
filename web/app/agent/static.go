8963edc6186f4f163bbfd33a7b2216643e1e7671
Oleg Sidorov <me@whitebox.io>
2019-05-15 22:19:49 +0200

    Some css fixes for jsonTree

--- web/app/agent/static.go
+++ web/app/agent/static.go
@@ -11,4 +11,10 @@ func init() {
 			http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static"))),
 		},
 	)
+	RegisterWebAgent(
+		&DummyWebAgent{
+			"/favicon.ico",
+			http.FileServer(http.Dir("./web/static/img")),
+		},
+	)
 }
