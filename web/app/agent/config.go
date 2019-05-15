fa2e6c8c830b0bfa85569f08357de3cb2bc30f80
Oleg Sidorov <me@whitebox.io>
2019-05-15 09:21:09 +0200

    Implemented helpers for web admin and downloaded assets locally

--- web/app/agent/config.go
+++ web/app/agent/config.go
@@ -1,7 +1,6 @@
 package agent
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/awesome-flow/flow/pkg/cfg"
@@ -20,15 +19,7 @@ func init() {
 					return
 				}
 				cfgdata := repo.(*cfg.Repository).Explain()
-				data, err := json.Marshal(cfgdata)
-				if err != nil {
-					rw.WriteHeader(http.StatusInternalServerError)
-					rw.Write([]byte(err.Error()))
-					return
-				}
-				rw.Header().Add("Content-Type", "application/json")
-				rw.WriteHeader(http.StatusOK)
-				rw.Write(data)
+				respondWith(rw, RespJson, "", cfgdata)
 			},
 		),
 	)
--- web/app/agent/index.go
+++ web/app/agent/index.go
@@ -1,26 +1,91 @@
 package agent
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"net/http"
-	"sort"
 )
 
 var (
-	TmplPage *template.Template
+	tmpl *template.Template
 )
 
 func init() {
 	var err error
-	TmplPage, err = template.ParseFiles("web/template/page.gohtml")
+	tmpl, err = template.ParseFiles(
+		"web/template/page.gohtml",
+		"web/template/page/index.gohtml",
+		"web/template/page/graphviz.gohtml",
+	)
 	if err != nil {
 		panic(err.Error())
 	}
 }
 
 type Page struct {
+	Data interface{}
+	Body template.HTML
+}
+
+type ResponseFormat uint8
+
+const (
+	RespHtml ResponseFormat = iota
+	RespJson
+)
+
+const (
+	HdrContentType  = "Content-Type"
+	ContentTypeHtml = "text/html"
+	ContentTypeJson = "application/json"
+)
+
+func respondWith(rw http.ResponseWriter, format ResponseFormat, tmplName string, data interface{}) error {
+	var err error
+	switch format {
+	case RespHtml:
+		err = respondWithHtml(rw, tmplName, data)
+	case RespJson:
+		err = respondWithJson(rw, data)
+	default:
+		err = fmt.Errorf("Unknown response format: %d", format)
+	}
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write([]byte(fmt.Sprintf("Internal Server Error: %s", err)))
+		return err
+	}
+	return nil
+}
+
+func respondWithHtml(rw http.ResponseWriter, tmplName string, data interface{}) error {
+	rw.Header().Add(HdrContentType, ContentTypeHtml)
+	bw := bytes.NewBuffer(nil)
+	if err := tmpl.ExecuteTemplate(bw, tmplName, data); err != nil {
+		return err
+	}
+	if err := tmpl.ExecuteTemplate(rw, "layout", &Page{Data: data, Body: template.HTML(bw.Bytes())}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func respondWithJson(rw http.ResponseWriter, data interface{}) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	rw.Header().Add(HdrContentType, ContentTypeJson)
+	if _, err := rw.Write(js); err != nil {
+		return err
+	}
+	return nil
+}
+
+type IndexPage struct {
 	Title string
-	Body  string
 }
 
 func init() {
@@ -28,26 +93,7 @@ func init() {
 		NewDummyWebAgent(
 			"/",
 			func(rw http.ResponseWriter, req *http.Request) {
-				sort.Sort(webAgents)
-				page := &Page{
-					Title: "Flow admin interface",
-					Body:  "Hello Flow!",
-				}
-				TmplPage.Execute(rw, page)
-				// rw.WriteHeader(http.StatusOK)
-
-				// rw.Write([]byte("<html><body><h1>flowd admin interface</h1><ul>"))
-				// var path string
-				// for _, agent := range webAgents {
-				// 	path = agent.GetPath()
-				// 	if path == "/" {
-				// 		// do not register ourselves
-				// 		continue
-				// 	}
-				// 	rw.Write([]byte(fmt.Sprintf("<li><a href='%s'>%[1]s</a></li>",
-				// 		path)))
-				// }
-				// rw.Write([]byte("</ul></body></html>"))
+				respondWith(rw, RespHtml, "index", &IndexPage{Title: "Flow admin interface"})
 			},
 		),
 	)
